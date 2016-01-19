d41aec98c8936cd970f6d6f624b0bf792634a0a7
Anu Rajaram <anurajaram23@gmail.com>
2016-01-19 12:52:37 -0500

    R program
    Predictive analysis for airbnb kaggle competition, to estimate which
    destination users want to travel, based on their language and gender.

---
+++ lang-subsets.R
@@ -0,0 +1,29 @@
+subset(mtcars, mpg>25)
+
+
+au <- subset(data, country_destination == "AU")
+prop.table(table(au$language))
+
+ko_lang <- subset(data, language == "ko")
+
+zh_lang <- subset(data, language == "zh")
+
+da_lang <- subset(data, language == "da")
+de_lang <- subset(data, language == "de")
+el_lang <- subset(data, language == "el")
+es_lang <- subset(data, language == "es")
+en_lang <- subset(data, language == "en")
+fr_lang <- subset(data, language == "fr")
+zh_lang <- subset(data, language == "zh")
+is_lang <- subset(data, language == "is")
+tr_lang <- subset(data, language == "tr")
+it_lang <- subset(data, language == "it")
+ja_lang <- subset(data, language == "ja")
+nl_lang <- subset(data, language == "nl")
+no_lang <- subset(data, language == "no")
+pl_lang <- subset(data, language == "pl")
+pt_lang <- subset(data, language == "pt")
+ru_lang <- subset(data, language == "ru")
+sv_lang <- subset(data, language == "sv")
+ca_lang <- subset(data, language == "ca")
+cs_lang <- subset(data, language == "cs")
